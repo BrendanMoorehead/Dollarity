249475e6aa60b3b433e5d8c219e7a047e62b49ee
Brendan Moorehead <brendan.moorehead11@gmail.com>
2024-05-06 22:09:31 -0400

    Additionaly chart addons

--- dollarity/src/Components/Charts/AverageSpendingCard.js
+++ dollarity/src/Components/Charts/AverageSpendingCard.js
@@ -1,9 +1,41 @@
 import React from 'react'
 import AverageSpendingChart from './AverageSpendingChart'
+import { Select, Space } from 'antd';
+
+const selectData = [
+    {
+        value: 'This Month',
+        label: 'This Month'
+    },
+    {
+        value: 'Last Month',
+        label: 'Last Month'
+    },
+    {
+        value: 'Average',
+        label: 'Average'
+    }
+]
+
 const AverageSpendingCard = () => {
   return (
     <div style={styles.card}>
-        <p>Spending</p>
+        <div style={styles.headerWrapper}>
+            <p style={styles.header}>Spending</p>
+            <div style={styles.selectWrapper}>
+                <Select 
+                dropdownStyle={{ backgroundColor: 'green' }}
+                style={styles.firstSelect}
+                defaultValue={'This Month'}
+                options={selectData}
+                />
+                <Select 
+                style={{ width: 140}}
+                defaultValue={'Last Month'}
+                options={selectData}
+                />
+            </div>
+        </div>
         <AverageSpendingChart />
     </div>
   )
@@ -12,6 +44,23 @@ const AverageSpendingCard = () => {
 const styles = ({
     card: {
         margin: 30
+    },
+    header: {
+        fontWeight: 'bold',
+        fontSize: '1.2rem'
+    },
+    headerWrapper: {
+        display: 'flex', 
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    selectWrapper: {
+        display: 'flex',
+        gap: '10px'
+    },
+    firstSelect: {
+        width: 140,
+        border: 'green'
     }
 })
 
